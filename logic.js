5692fabd3c10d04da68289b62fb45849e7efbba8
SerenaZhaoYanqing <87590423+SerenaZhaoYanqing@users.noreply.github.com>
2021-12-01 16:37:43 +1100

    layer all donw

--- logic.js
+++ logic.js
@@ -5,32 +5,37 @@ var queryUrl="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week
 var lineUrl="https://github.com/fraxen/tectonicplates/blob/master/GeoJSON/PB2002_plates.json";
 
  //************************** Layers****************************
-//Refer W17.1.9 Activity step 1-7: Create multiple layers, set default display)
+//1. create all three layers (Refer W17.1.9 Activity step 1-7: Create multiple layers, set default display)
 //https://stackoverflow.com/questions/37166172/mapbox-tiles-and-leafletjs (mapbox id type)
 
-// 1.Create two separate layer groups: one for earthquakes and one for tectonic plates
-var earthquakes = L.layerGroup();
-var tectonicplates = L.layerGroup();
-// 2. create all three layers (
-var satellitemap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",{
+var satellitemap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    id: "satellite-v9",
+    maxZoom: 9,
+    zoomOffset: -1,
+    id: "mapbox.satellite",
     accessToken: API_KEY
   });
   
-  var grayscaleMap =L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",{
+  var grayscaleMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
     id: "light-v10",
     accessToken: API_KEY
   });
   
-  var outdoormap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",{
+  var outdoormap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
       attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-      id: "outdoors-v11",
+      tileSize: 512,
+      maxZoom: 18,
+      zoomOffset: -1,
+      id: "mapbox/streets-v11",
       accessToken: API_KEY
   });
   
-
+    // 2.Create two separate layer groups: one for earthquakes and one for tectonic plates
+  var earthquakes = L.layerGroup();
+  var tectonicplates = L.layerGroup();
+  
   
   // 3.Create a baseMaps object
   var baseMaps = {
@@ -59,7 +64,7 @@ var satellitemap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/til
 
   //**************************Markers****************************
 // Perform a GET request to the query URL
-d3.json(queryUrl),function(earthquakeData) {
+d3.json(queryUrl),function(earthdata) {
     //creating function to determine size of markers based on value of the magnitute
     function markerSize(mag){
     // in case mag =0, 
@@ -68,26 +73,25 @@ d3.json(queryUrl),function(earthquakeData) {
         }
         return mag*10;
     }
-
 // create  function to determine the color of the markers based on value of the magnitute
 //https://gis.stackexchange.com/questions/322535/custom-marker-colours-in-leaflet-based-on-attribute
 //https://www.color-hex.com/ (reference for color) //https://www.color-hex.com/color-palette/4699
 
-function colorscale(mag){
-  switch(true){
-      case mag > 5:
-      return "#ff0000";
-      case mag > 4:
-      return "#ff4d00";
-      case mag > 3:
-      return "#ff7400";
-      case mag > 2:
-      return "#ffc100";
-      case mag > 0:
-      return "#baffc9";
-   }
-  }
-// final style of markers ( define size and color, set two different function)
+    function colorscale(mag){
+        switch(true){
+            case mag > 5:
+            return "#ff0000";
+            case mag > 4:
+            return "#ff4d00";
+            case mag > 3:
+            return "#ff7400";
+            case mag > 2:
+            return "#ffc100";
+            case mag > 0:
+            return "#baffc9";
+         }
+        }
+// create new function with finalised markers with above functions ( define size and color)
         function finalMarkers(feature){
             return{
             fillOpacity: 0.75,
@@ -96,19 +100,14 @@ function colorscale(mag){
             radius: markerSize(feature.properties.mag)   
             }
         }
-      }
-
-
-
-
 // refer 17.1.10 activity 
  // Perform a GET request to the query URL 
-d3.json(queryUrl).then(function(earthquakeData) {
+d3.json(queryUrl).then(function(earthdata) {
     // Once we get a response, send the data.features object to the createFeatures function
-    createFeatures(earthquakeData.features);
+    createFeatures(data.features);
   })
   
-  function createFeatures(earthquakeData) {
+  function createFeatures(earthdata) {
     // Define a function we want to run once for each feature in the features array
     // Give each feature a popup describing the place and time of the earthquake
     function onEachFeature(feature, layer) {
@@ -119,7 +118,7 @@ d3.json(queryUrl).then(function(earthquakeData) {
     // Run the onEachFeature function once for each piece of data in the array
     //https://leafletjs.com/reference.html#marker
     //https://leafletjs.com/reference.html#geojson-pointtolayer
-    var earthquakes = L.geoJSON(earthquakeData, {
+    var earthquakes = L.geoJSON(earthdata, {
       onEachFeature: onEachFeature,
       style: finalMarkers,
       pointToLayer: function(feature, latlng) {
@@ -154,6 +153,4 @@ d3.json(lineUrl, function(tectonicdata){
         return div;
     };
     // Add Legend to the Map
-    legend.addTo(myMap);
-  };
-  
+    legend.addTo(myMap);})
